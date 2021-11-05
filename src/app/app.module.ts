77caff7bf1d7ead35948f892a189dd8a721ca758
ManulMax <manulmadara9725@gmail.com>
2021-11-05 17:45:15 +0530

    conflict changes 01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/login/home.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
