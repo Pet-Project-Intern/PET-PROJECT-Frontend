e15376295a7c495bb08df84985b43c0e04e0fb76
ManulMax <manulmadara9725@gmail.com>
2021-11-09 03:26:18 +0530

    use shared module with login and signup pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { UserInfoComponent } from './component/user-info/user-info.component';
 import { HomeComponent } from './component/login/home.component';
+import { DefaultComponent } from './layouts/default/default.component';
+import { SignupComponent } from './component/signup/signup.component';
 
 const routes: Routes = [
-  { path: '', component: UserInfoComponent },
-  { path: 'login', component: HomeComponent },
+  // { path: '', component: UserInfoComponent },
+  // { path: 'login', component: HomeComponent },
+  {
+    path:'',
+    component: DefaultComponent,
+    children:[{
+      path:'',
+      component: HomeComponent
+    },{
+      path:'signup',
+      component: SignupComponent
+    }]
+  }
 ];
 
 @NgModule({
