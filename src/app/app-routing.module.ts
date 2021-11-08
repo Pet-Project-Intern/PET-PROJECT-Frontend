abd7fddd030836ad0f8e95fc40d74919f600db1f
LakshanAmal <lakshanamal100@gmail.com>
2021-11-08 14:22:06 +0530

    bug fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserInfoComponent } from './component/user-info/user-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'user-info', component: UserInfoComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
