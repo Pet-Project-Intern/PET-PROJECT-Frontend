d6fc474101e1b1f6fae35e7c1e186e31fe0cc5ac
LakshanAmal <lakshanamal100@gmail.com>
2021-11-08 14:29:06 +0530

    router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserInfoComponent } from './component/user-info/user-info.component';
+import { HomeComponent } from './component/login/home.component';
 
-const routes: Routes = [{ path: 'user-info', component: UserInfoComponent }];
+const routes: Routes = [
+  { path: 'user-info', component: UserInfoComponent },
+  { path: 'login', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
