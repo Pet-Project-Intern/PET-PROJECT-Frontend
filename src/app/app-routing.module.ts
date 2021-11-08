61a236c976c163a82e7e9f395d8a392ff06c0b1c
ManulMax <manulmadara9725@gmail.com>
2021-11-08 14:52:48 +0530

    signup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './component/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path : 'signup',
+  component : SignupComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
