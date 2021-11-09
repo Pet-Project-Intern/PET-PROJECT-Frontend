6f543ac4937524de2b25cf5856e7434fe56bea3c
ManulMax <manulmadara9725@gmail.com>
2021-11-09 15:01:31 +0530

    shared component and layout complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './component/login/home.component';
+import { HomeComponent } from './component/home/home.component';
 import { SignupComponent } from './component/signup/signup.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SideNavComponent } from './component/side-nav/side-nav.component';
@@ -16,7 +16,7 @@ import { UserInfoComponent } from './component/user-info/user-info.component';
 import { NavComponent } from './component/nav/nav.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { PopupNewuserComponent } from './component/popup-newuser/popup-newuser.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
@@ -52,6 +52,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatNativeDateModule,
     MatSelectModule,
     FlexLayoutModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
